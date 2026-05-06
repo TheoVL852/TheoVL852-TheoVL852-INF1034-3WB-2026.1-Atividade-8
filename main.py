972ec987c4e8a0d37563d696b856d1ada2e47778
TheoVL852 <theovisconti@icloud.com>
2026-05-06 12:51:54 -0300

    Continuando no pygame

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from pygame import *
+import sys
+
 def valida_email(email):
     return email[-8:]=='@puc.com'
 
@@ -25,9 +28,6 @@ def valida_senha(senha):
     check_minuscula = possuiMinuscula(senha)
     check_numero = possuiNumero(senha)
     return check_tamanho and check_maiuscula and check_minuscula and check_numero
-
-
-print(valida_email('theovisconti@icloud.com'))
 
 # 1) Pegar a letra e converter para decimal ('Z'-->90)
 # 2) Subtrair o valor decimal de 65 ('B' --> 90-65-->25)
@@ -65,3 +65,77 @@ def cripotografa_senha(senha):
         else:
             senha_cripto+=char
     return senha_cripto
+
+init()
+
+window = display.set_mode((1280,720))
+running = True
+clock = time.Clock()
+texto = font.Font(None, 25)
+
+email = '' 
+emailescrito = texto.render(email,True, (255,255,255))
+senha = ''
+senhaescrita = texto.render(senha,True,(255,255,255))
+limpa = False
+
+while running:
+    clock.tick(60)
+
+    ##UPDATE
+
+    dt = clock.get_time()/1000
+    keys = key.get_pressed()
+
+
+    for ev in event.get():
+        if ev.type == QUIT:
+            running = False
+        if ev.type == KEYDOWN:
+            if ev.key == K_BACKSPACE:
+                email=email[:-1]
+                emailescrito=texto.render(email, True, (255,255,255))
+            if ev.key == K_RETURN:
+                print(email)
+                valida_email(email)
+                if valida_email(email) == True: #Se o email for valido, passar para a senha
+                    limpa = True
+                    email = ''
+                if ev.key == K_BACKSPACE:
+                    senha=senha[:-1]
+                    senhaescrita=texto.render(senha,True, (255,255,255))
+                if ev.key == K_RETURN:
+                    print(senha)
+                if ev.type == TEXTINPUT:
+                    senha += ev.text
+                    senhaescrita = texto.render(senha, True, (255,255,255))
+                    window.blit(senhaescrita,(310,350))
+
+            
+                
+                
+        if ev.type == TEXTINPUT:
+            email += ev.text
+            emailescrito = texto.render(email, True, (255,255,255))
+
+    window.fill((0,0,0))
+
+    draw.rect(window, (255, 255, 255), (300,300,500,100), 4)
+    escrito = texto.render('Email:', True , (255,255,255))
+    window.blit(escrito, (310,310))
+    window.blit(emailescrito,(310,350))
+    
+
+
+    if limpa == True: #Para limpar tudo, o retangulo que cobre tem que ser desenhado fora do ev.key
+        draw.rect(window,(0,0,0), (0,0,1280,720))
+        draw.rect(window, (255, 255, 255), (300,300,500,100), 4)
+        escrito = texto.render('Senha:', True , (255,255,255)) #Pedindo senha
+        window.blit(escrito,(310,310))
+        
+        
+
+        
+
+
+    display.update()
